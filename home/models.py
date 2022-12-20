bf762c9ade6e3155c771931de4573e0b3bce2c40
Sitamdu <Beekramrai22@gmail.com>
2022-12-20 23:18:38 +0545

    dyanmicpageswithmodels&views

--- home/models.py
+++ home/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=300)
+    subject = models.TextField(blank = True)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Information(models.Model):
+    address1 = models.CharField(max_length=300)
+    address2 = models.CharField(max_length=300)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    time = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.address1
+
+class Service(models.Model):
+    title = models.CharField(max_length=200)
+    logo = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Blogsingle(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=300)
+    subject = models.TextField(blank=True)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Element(models.Model):
+    number = models.CharField(max_length=100)
+    country = models.CharField(max_length=200)
+    visit = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.country
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=300)
+    post = models.CharField(max_length=400)
+    image = models.TextField()
+    comment = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Subscribe(models.Model):
+    email = models.EmailField(max_length=200)
+
+    def __str__(self):
+        return self.email
+
+
+
+
+
+
+
--- home/views.py
+++ home/views.py
@@ -1,12 +1,41 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    views = {}
+    views['services'] = Service.objects.all()
+    views['feedbacks'] = Feedback.objects.all()
+    if request.method == 'POST':
+        email = request.POST['EMAIL']
+        data = Subscribe.objects.create(
+            email = email
+        )
+        data.save()
+        return render(request, 'index.html')
+
+    return render(request, 'index.html',views)
 def about(request):
     return render(request, 'about.html')
 def contact(request):
-    return render(request, 'contact.html')
+    views = {}
+    views['infos'] = Information.objects.all()
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        data = Contact.objects.create(
+            name = name,
+            email = email,
+            subject = subject,
+            message = message
+        )
+        data.save()
+        views['mess'] = 'The message is Submitted!'
+        return render(request, 'contact.html',views)
+
+    return render(request, 'contact.html',views)
 def portfolio(request):
     return render(request, 'portfolio.html')
 def services(request):
@@ -16,8 +45,27 @@ def price(request):
 def blog_home(request):
     return render(request, 'blog-home.html')
 def blog_single(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        data = Blogsingle.objects.create(
+            name = name,
+            email = email,
+            subject = subject,
+            message = message
+        )
+        data.save()
+        messages = {'mess':'The message is submitted!'}
+        return render(request, 'blog-single.html',messages)
+
     return render(request, 'blog-single.html')
 def elements(request):
-    return render(request, 'elements.html')
+    views = {}
+    views['countries'] = Element.objects.all()
+
+    return render(request, 'elements.html',views)
+
 
 
